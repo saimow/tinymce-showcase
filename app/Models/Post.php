9558954611219c3344cde0d21f4a87c9187e449f
Mohamed Hafidi <simodev8@gmail.com>
2022-11-23 18:45:36 +0100

    Remove Spatie setup

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,24 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-use Spatie\Image\Manipulations;
-use Spatie\MediaLibrary\HasMedia;
-use Spatie\MediaLibrary\InteractsWithMedia;
-use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
-class Post extends Model implements HasMedia
+class Post extends Model 
 {
     use HasFactory;
-    use InteractsWithMedia;
 
     protected $fillable = [
         'title',
         'description'
     ];
 
-    public function registerMediaConversions(Media $media = null): void
-    {
-        $this->addMediaConversion('thumb')
-              ->fit(Manipulations::FIT_MAX, 1000, 1000);
-    }
 }
